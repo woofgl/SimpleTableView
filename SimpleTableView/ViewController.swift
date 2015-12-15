4b3bf89d9b8d27e0f1000d08c3e45585220dfe1a
woofgl <woofgl@gmail.com>
2015-12-15 16:55:37 +0800

    . simple table view demo

--- SimpleTableView/ViewController.swift
+++ SimpleTableView/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //Add some data to display
+    var items: [String] = ["Egg Benedict", "Mushroom Risotto", "Full Breakfast", "Hamburger", "Ham and Egg Sandwich", "Creme Brelee", "White Chocolate Donut", "Starbucks Coffee", "Vegetable Curry", "Instant Noodle with Egg", "Noodle with BBQ Pork", "Japanese Noodle with Pork", "Green Tea", "Thai Shrimp Cake", "Angry Birds Cake", "Ham and Cheese Panini", ""]
+    
+    //Set the number of rows
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.items.count;
+    }
+    
+    //Create the cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        var identifier = "cell"
+        
+        if indexPath.row % 2 == 1{
+            identifier = "cell2"
+        }
+        
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier(identifier)! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        cell.imageView?.image = UIImage(named: "p1.jpg")
+        
+        return cell
+    }
+    
+    //table view cell height
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0;
+    }
+    
+    //event for table view
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        //        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        print("You selected cell #\(indexPath.row)!")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
